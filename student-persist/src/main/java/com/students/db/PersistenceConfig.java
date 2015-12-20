bfaf01a1fe15020d92fc7d9d67209f9ec28ffdfd
Daniel Christo <danielchristo@gmail.com>
2015-12-19 21:48:42 -0500

    removed some unnessary files

--- student-persist/src/main/java/com/students/db/PersistenceConfig.java
+++ student-persist/src/main/java/com/students/db/PersistenceConfig.java
@@ -1,33 +1,35 @@
 package com.students.db;
 
 import java.util.Properties;
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
 @EnableConfigurationProperties
 @EnableAutoConfiguration
+@ComponentScan(basePackages = {"com.students"})
+@EnableJpaRepositories(basePackages = "com.students.db.dao")
+@EntityScan(basePackages = {"com.students.db"})
 @PropertySource("classpath:persistence.properties")
 public class PersistenceConfig {
 
 	// Create a bean factory for creating JPA entity manager factories to be injected and used by DAO objects.
-	// Populate it with JPA properties from the persistence.properties
+	// Populate it with JPA properties from the persistence.properties file
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
 
@@ -43,7 +45,8 @@ public class PersistenceConfig {
     }
 
     // Create a bean which is a factory for creating data source connections. In this case populate it
-    // With datasource configuration for an HSQL database 
+    // With datasource configuration for an HSQL database (configuration is set in spring.datasource variables
+    // in persistence.properties file
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource getDataSource() {
@@ -56,25 +59,5 @@ public class PersistenceConfig {
     public Properties jpaProperties() {
         return new Properties();
     }
-
-    // Load JNDI properties with data source properties
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public Properties jndiEntryNames() {
-        return new Properties();
-    }
-
-    // Set up spring transaction infrastructure 
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(emf);
-        return transactionManager;
-    }
-
-    // Translates native resource exceptions to Spring's DataAccessException hierarchy.
-    @Bean
-    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
-        return new PersistenceExceptionTranslationPostProcessor();
-    }
+    
 }
--- student-persist/src/main/java/com/students/db/service/StudentService.java
+++ student-persist/src/main/java/com/students/db/service/StudentService.java
@@ -1,12 +1,10 @@
 package com.students.db.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.students.db.dao.StudentRepository;
 import com.students.db.entity.Student;
-import com.students.db.manager.StudentManager;
 
 // Create a service to call for database operations 
 
@@ -14,11 +12,10 @@ import com.students.db.manager.StudentManager;
 public class StudentService {
 
 	@Autowired
-	StudentManager studentManager;
+	StudentRepository studentRepository;
 	
-	// Initial operation to get all students
-	public Student getAllStudents(String id) {
-		return studentManager.getStudents(id);
+	public Student getStudents(String id) {
+		return studentRepository.findOne(id);
 	}
 	
 }
