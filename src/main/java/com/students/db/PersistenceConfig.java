5bc82125dcca6b95ad30cb287ce0daf717f02136
Daniel Christo <danielchristo@gmail.com>
2016-01-09 13:54:24 -0500

    Cleaned up - removed unnecessary properties

--- src/main/java/com/students/db/PersistenceConfig.java
+++ src/main/java/com/students/db/PersistenceConfig.java
@@ -2,13 +2,9 @@ package com.students.db;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
-
 import javax.sql.DataSource;
 
-import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -16,12 +12,10 @@ import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -42,6 +36,17 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 @PropertySource("classpath:persistence.properties")
 public class PersistenceConfig {
 	
+	//*************************************************
+	// Persistence Configuration
+	
+    // Database connection
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource getDataSource() {
+        return new DriverManagerDataSource();
+    }
+	
+	// Spring's database transaction management bean injected with Database connection from above
 	@Bean
 	public PlatformTransactionManager transactionManager() {
 		DataSourceTransactionManager txManager = new DataSourceTransactionManager();
@@ -49,6 +54,19 @@ public class PersistenceConfig {
 		return txManager;
 	}
 	
+	// Database entity manager set to use Java Persistence API with Hibernate as the persistence provider on
+	// the above data source
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+
+        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
+        entityManager.setDataSource(getDataSource());
+        entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        return entityManager;
+    }
+    
+    // Filter that injects the entity manager as a servlet view
 	@Bean
 	public OpenEntityManagerInViewFilter viewFilter() {
 		OpenEntityManagerInViewFilter viewFilter = new OpenEntityManagerInViewFilter();
@@ -56,70 +74,10 @@ public class PersistenceConfig {
 		return viewFilter;
 	}
 	
-	// Create a bean factory for creating JPA entity manager factories to be injected and used by DAO objects.
-	// Populate it with JPA properties from the persistence.properties file
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-
-        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
-        entityManager.setDataSource(getDataSource());
-        entityManager.setJpaVendorAdapter(vendorAdapter());
-
-        return entityManager;
-    }
-    
-    @Bean
-    public JpaVendorAdapter vendorAdapter() {
-    	JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-    	return vendorAdapter;
-    }
-
-    // Create a bean which is a factory for creating data source connections. In this case populate it
-    // With datasource configuration for an HSQL database (configuration is set in spring.datasource variables
-    // in persistence.properties file
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource getDataSource() {
-        return new DriverManagerDataSource();
-    }
-
-    // Bean for retrieving JPA properties - JPA properties are set to work with HSQL 
-    @Bean
-    @ConfigurationProperties(prefix = "spring.jpa")
-    public Properties properties() {
-        return new Properties();
-    }
-    
-    @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
-        return tomcat;
-    }
-    
-	@Bean
-	public ServerProperties serverProperties () {
-		ServerProperties serverProperties = new ServerProperties();
-		serverProperties.setContextPath("");
-		serverProperties.setPort(8080);
-		return serverProperties;
-	}
+	//*************************************************
+	// Servlet Configuration
 	
-	// Persistence Exception 
-	@Bean
-	PersistenceExceptionTranslationPostProcessor exceptionProcessor() {
-		PersistenceExceptionTranslationPostProcessor exceptionProcessor = new PersistenceExceptionTranslationPostProcessor();
-		return exceptionProcessor;
-	}
-	
-	// Dispatch Servlet setup and beans
-	
-	@Bean
-	ServletRegistrationBean servletRegistration() {
-		ServletRegistrationBean servletRegistration = new ServletRegistrationBean();
-		servletRegistration.setServlet(servlet());
-		return servletRegistration;
-	}
-	
+	// Dispatch Servlet
 	@Bean
 	DispatcherServlet servlet() {
 	       AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
@@ -127,13 +85,24 @@ public class PersistenceConfig {
 	       return servlet;
 	}
 	
-	// Request mapping handler and adapter to interpret and use controller annotations
+	// Embedded Servlet Container
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        return tomcat;
+    }
+	
+	//*************************************************
+	// Web Model View Controller Configuration
+    
+	// Creates RequestMappingInfo instances from type and method-level @RequestMapping annotations in @Controller classes.
 	@Bean
 	public RequestMappingHandlerMapping handlerMapping() {
 		RequestMappingHandlerMapping handlerMapping = new RequestMappingHandlerMapping();
 		return handlerMapping;
 	}
 	
+	// Adapter for request mappings - e.g. adds converter adapters to go from json to java object and vice versa
 	@Bean
 	public RequestMappingHandlerAdapter handlerAdapter() {
 		RequestMappingHandlerAdapter handlerAdapter = new RequestMappingHandlerAdapter();
